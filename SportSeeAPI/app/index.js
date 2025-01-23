142d561da51748431cdb46aeeb1aa072ff619f9f
Vanda89 <sandrinealcazar@gmail.com>
2025-01-23 13:03:56 +0100

    feat(backend): Add configuration for production server

--- SportSeeAPI/app/index.js
+++ SportSeeAPI/app/index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const cors = require('cors')
-
 const router = require('./routes')
 
 const app = express()
 app.use(cors())
-const port = 3000
+
+const PORT = process.env.PORT || 3002
+const API_HOST = process.env.API_HOST || 'localhost'
 
 app.use(router)
 
-app.listen(port, () => console.log(`Magic happens on port ${port}`))
+app.listen(PORT, () => {
+	console.log(`Server listening on http://${API_HOST}:${PORT}`)
+})
